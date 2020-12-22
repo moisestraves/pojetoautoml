7aaeb8a0739e49200363f836bf4adb2c2e083742
Moisés Traves <moisestraves.mt@gmail.com>
2020-12-22 18:27:46 -0300

    Inserido Html Cadastro de Usuário

---
+++ cadastrar-usuario.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/main.css"  >
+    <title>Projeto AutoMl</title>
+</head>
+<header>
+<h2>AutoMl</h2>
+</header>
+<body>
+    <div class="container">
+   
+        <div class="login">
+    <form  method="POST" action="">
+        <h5 class="text-center text-success">Cadastro de Usuário</h5>
+ 
+    <input type="text"  name="nome" placeholder="NOME DO USUÁRIO" required ><br>
+    <input type="e-mail"  name="login"placeholder="LOGIN" required ><br>
+    <input type="password" name="senha" placeholder="SENHA" required><br>
+
+    <select name="pefil" id="perfilusuario" required>
+        <option value="0">É ADMINISTRADOR DO SISTEMA ?</option>
+        <option value="1">N</option>
+        <option value="2">S</option>
+</select>
+   
+    
+   
+  <input style="background-color: black; color:white; " type="submit" value="cadastrar">
+    </form>
+        </div>
+</div>
+</body>
+</html>
+
+
+
+    
